22620f16aae5742c7af5979ec879365e54a28cdd
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-22 00:04:09 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -5,6 +5,11 @@ def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
 end
 
+<<<<<<< HEAD
 def play_game(name1,name2)
   "#{name1} is better than #{name2}"
+=======
+def play_game
+  "#{player1} is better than #{player2}"
+>>>>>>> 17f4806f89b2f18230b2f92d7d3b4669a709db21
 end
